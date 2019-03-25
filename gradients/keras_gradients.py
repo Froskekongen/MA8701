8e671c893f9f08f5d41ccf2570c6bf27ddaa796d
Erlend Aune <erlend.aune.1983@gmail.com>
2019-03-25 16:48:34 +0100

    Getting gradients.

--- gradients/keras_gradients.py
+++ gradients/keras_gradients.py
@@ -10,7 +10,6 @@ import numpy as np
 
 
 model = Sequential()
-model.add(Dense(12, input_dim=8, activation='relu'))
 model.add(Dense(8, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
